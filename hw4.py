fb74c52bd4265a4779aece7cc224e26f932f7aa7
ayyshh <107502844+ayyshh@users.noreply.github.com>
2022-08-13 21:16:27 +0100

    Update hw4.py

--- hw4.py
+++ hw4.py
@@ -1,4 +1,14 @@
-def search_in_sorted_matrix(matrix, target):
+matrix = [
+[1,4,7,12,15,1000],
+[2,5,19,31,32,1001],
+[3,8,24,33,35,1002],
+[40,41,42,44,45,1003],
+[99,100,103,106,128,1004]
+]
+
+target =44
+
+def searching_matrix(matrix, target):
     COLUMN = len(matrix[0]) - 1
     ROW = 0
 
@@ -10,3 +20,4 @@ def search_in_sorted_matrix(matrix, target):
         else:
             return [ROW, COLUMN]
     return [-1, -1]
+    
